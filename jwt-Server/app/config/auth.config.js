ac5c1fb3954720323557c8bda5c55543b3425986
VK dev <vk@jcdev.com>
2022-07-25 00:41:02 +0530

    Initial Server Commit

---
+++ jwt-Server/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "vkna-secret-key"
+  };
---
+++ jwt-Server/app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "databases-auth.000webhost.com",
+    USER: "id19280859_vknaadminuser",
+    PASSWORD: "~U3@k?!uYHq9pLk",
+    DB: "id19280859_vknadb",
+    PORT : 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
