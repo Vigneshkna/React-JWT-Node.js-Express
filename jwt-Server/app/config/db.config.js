d931903164bfde456d26658312d8dc62eda11619
VK dev <vk@jcdev.com>
2022-07-25 00:51:29 +0530

    Update db config

--- jwt-Server/app/config/db.config.js
+++ jwt-Server/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "databases-auth.000webhost.com",
-    USER: "id19280859_vknaadminuser",
-    PASSWORD: "~U3@k?!uYHq9pLk",
-    DB: "id19280859_vknadb",
+    USER: "id19280859_vknaadminuser2100h",
+    PASSWORD: "~U3@k?!ualmYHq9pLkalpha",
+    DB: "id19280859_vknaadbadmin",
     PORT : 3306,
     dialect: "mysql",
     pool: {
